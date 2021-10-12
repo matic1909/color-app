06bf1f66e8dd8cbc532ca2ecf185af96cb84282b
Nils Matic <matic1909@gmail.com>
2021-10-12 19:27:37 +0200

    added palette component

---
+++ src/components/Palette.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Palette() {
+  return (
+    <div className="Palette">
+      {/* Navbar goes here */}
+      <div className="Palette-colors">{/* bunch of color boxes */}</div>
+      {/* footer eventually */}
+    </div>
+  );
+}
