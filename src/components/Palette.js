e299e65bfb4efb686f92809fca3aad21eda3bb34
Nils Matic <matic1909@gmail.com>
2021-10-13 18:53:49 +0200

    added footer, fixed scrollbars appearing

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -15,9 +15,9 @@ export default function Palette({ palette }) {
     setFormat(val);
   };
 
-  const { colors } = palette;
+  const { colors, paletteName, emoji } = palette;
   const colorBoxes = colors[level].map((color) => (
-    <ColorBox background={color[format]} name={color.name} />
+    <ColorBox background={color[format]} name={color.name} key={color.id} />
   ));
   return (
     <div className="Palette">
@@ -27,7 +27,10 @@ export default function Palette({ palette }) {
         handleFormatChange={changeColorFormat}
       />
       <div className="Palette-colors">{colorBoxes}</div>
-      {/* footer eventually */}
+      <footer className="Palette-footer">
+        {paletteName}
+        <span className="emoji">{emoji}</span>
+      </footer>
     </div>
   );
 }
