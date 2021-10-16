32ece51b6d77d68b3421279cc66b3e8f76ed1083
Nils Matic <matic1909@gmail.com>
2021-10-16 11:50:21 +0200

    added Form to add new Color to Palette

--- src/components/NewPaletteForm/DraggableColorBox.js
+++ src/components/NewPaletteForm/DraggableColorBox.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as Styled from './styles';
 
-export default function DraggableColorBox({ color }) {
+export default function DraggableColorBox({ color, name }) {
   return (
-    <Styled.DraggableColorBox color={color}>{color}</Styled.DraggableColorBox>
+    <Styled.DraggableColorBox color={color}>{name}</Styled.DraggableColorBox>
   );
 }
