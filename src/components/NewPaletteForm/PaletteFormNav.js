4698d5a14387e0c4449fd831c02a5e3385382178
Nils Matic <matic1909@gmail.com>
2021-10-16 15:29:09 +0200

    refactor NewPaletteNav to own component

---
+++ src/components/NewPaletteForm/PaletteFormNav.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { Link } from 'react-router-dom';
+
+export default function PaletteFormNav({
+  classes,
+  open,
+  palettes,
+  handleSubmit,
+  handleDrawerOpen,
+}) {
+  const [newPaletteName, setNewPaletteName] = useState('');
+
+  useEffect(() => {
+    ValidatorForm.addValidationRule('isPaletteNameUnique', (value) =>
+      palettes.every(
+        ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
+      )
+    );
+  }, []);
+
+  return (
+    <div>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        color="default"
+        className={classNames(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar disableGutters={!open}>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            onClick={handleDrawerOpen}
+            className={classNames(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" noWrap>
+            Persistent drawer
+          </Typography>
+          <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
+            <TextValidator
+              label="Palette Name"
+              name="newPaletteName"
+              value={newPaletteName}
+              onChange={(e) => setNewPaletteName(e.target.value)}
+              validators={['required', 'isPaletteNameUnique']}
+              errorMessages={[
+                'Enter palette name',
+                'Palette name already used',
+              ]}
+            />
+            <Link to="/">
+              <Button variant="contained" color="secondary">
+                Go Back
+              </Button>
+            </Link>
+            <Button type="submit" variant="contained" color="primary">
+              Save Palette
+            </Button>
+          </ValidatorForm>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
